f301514a3e298bb0e57a7abeff3821c12ab2c5be
itseasy21 <itseasy21@users.noreply.github.com>
2014-08-25 11:18:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,54 @@
 
 ### Made by Shubham Mathur (itseasy21)
 
-echo '----------------------------------------'
-echo " Starting the W3TooLS Varnish installer"
-echo "--------------By itseasy21--------------"
-echo '-----------------------------------------'
+#-Colors :D
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+RESET='\033[0m'
 
-#--Adding Required FUnctions
+#--Clearing Your Screen
+clear
+
+echo -e "$GREEN----------------------------------------$RESET"
+echo -e " Starting the W3TooLS Varnish installer"
+echo -e "            Version 0.2             "
+echo -e "     http://www.w3tool.blogspot.in/  "
+echo -e "$GREEN----------------------------------------$RESET"
+
+#--Making Sure we have required libs
 cd /root
 yum install vim wget lynx sed rpm -y
+
+#--CPanel check
+echo -ne "Searchinng for cPanel .."
+if [ -e  "/usr/local/cpanel/version" ]; then
+	echo -e "[ $GREEN cPanel Found $RESET ]"
+else
+	echo -e "[ $RED cPanel Not Found $RESET ]"
+	exit
+fi
+
+#--Libs Check
+echo " "
+echo -ne "Checking for Yum .."
+if [ -e  "/etc/yum.conf" ]; then
+	echo -e "[ $GREEN Found $RESET ]"
+else
+	echo -e "[ $RED Not Found $RESET ]"
+	exit
+fi
+
+echo -ne "Checking for SED .."
+if [ -e  "/bin/sed" ]; then
+	echo -e "[ $GREEN Found $RESET ]"
+else
+	echo -e "[ $RED Not Found $RESET ]"
+	exit
+fi
+
+#--Removing Previous Varnish Install
+yum remove varnish -y
+
 
 #--Change port Apache listens on to 82
 cd /var/cpanel
@@ -33,11 +73,20 @@ cd /etc/sysconfig/
 sed -i 's#VARNISH_LISTEN_PORT=6081#VARNISH_LISTEN_PORT=80#g' ./varnish
 cd /etc/varnish/
 ip=$(( lynx --dump cpanel.net/showip.cgi ) 2>&1 | sed "s/ //g")
-#sed -i 's#host = "127.0.0.1"#host = "$ip"#g' ./default.vcl
 sed -i "s#host = \"127.0.0.1\"#host = \"$ip\"#g" ./default.vcl
 sed -i 's#port = "80"#port = "82"#g' ./default.vcl
 
 #-- Starting Varnish
 chkconfig varnish on
 service varnish start
+
+echo -e "$GREEN----------------------------------------$RESET"
+echo -e "$GREEN Varnish Cache Installation Complete  $RESET"
+echo -e "If varnish failed to install or you faced any bugs feel free to post them with install log at http://bit.ly/w3tools-varnish-issues"
+echo -e ""
+echo -e "You can monitor varnish cache through this monitoring tool:$GREEN varnishstat $RESET"
+echo -e ""
+echo -e "Thanks for using our Varnish Installer for CPanel"
+echo -e "For more stay tuned at our blog : http://www.w3tool.blogspot.in/"
+echo -e "$GREEN----------------------------------------$RESET"
 
